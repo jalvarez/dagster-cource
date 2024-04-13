b3d6d49de4f215b519111cd56bbde0373865e01c
Juan Miguel Alvarez Tosco <juanmi.alvarez@gmail.com>
2024-04-13 19:45:29 +0100

    feat: chapter 9

--- dagster_university/dagster_university/__init__.py
+++ dagster_university/dagster_university/__init__.py
@@ -2,22 +2,26 @@
 from dagster import Definitions, load_assets_from_modules
 
 from .resources import database_resource
-from .assets import metrics, trips
-from .jobs import trip_update_job, trips_by_week_update_job
+from .assets import metrics, trips, requests
+from .jobs import trip_update_job, trips_by_week_update_job, adhoc_request_job
 from .schedules import trip_update_schedule, weekly_update_schedule
-
+from .sensors import adhoc_request_sensor
 
 trip_assets = load_assets_from_modules([trips])
 metric_assets = load_assets_from_modules([metrics])
+request_assets = load_assets_from_modules([requests])
 
-all_jobs = [trip_update_job, trips_by_week_update_job]
+all_jobs = [trip_update_job, trips_by_week_update_job, adhoc_request_job]
 all_schedules = [trip_update_schedule, weekly_update_schedule]
+all_sensors = [adhoc_request_sensor]
+
 
 defs = Definitions(
-    assets=[*trip_assets, *metric_assets],
+    assets=[*trip_assets, *metric_assets, *request_assets],
     resources={
         "database": database_resource,
     },
     jobs=all_jobs,
     schedules=all_schedules,
+    sensors=all_sensors
 )
